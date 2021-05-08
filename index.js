1bc0fcb6af92007320e24adfb18b6b81ca60c701
marvin <marvin.mitchell@amarbank.co.id>
2021-05-08 12:56:52 +0700

    1-validation-and-hooks: add prehandler for root

--- index.js
+++ index.js
@@ -2,15 +2,37 @@
 const fastify = require("fastify")({ logger: true })
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        // requests needs to have a querystring with a `name` parameter
+        querystring: {
+            name: { type: 'string' }
+        },
+        // the response needs to be an object with `hello` properties of type string
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    },
+    prehandler: async (request, reply) => {
+        // E.g. check authentication
+    },
+    handler: async (request, reply) => {
+        return { hello: 'world' }
+    }
 })
 
 // Run the server!
-const start = async()=>{
-    try{
+const start = async () => {
+    try {
         await fastify.listen(3000)
-    } catch(err){
+    } catch (err) {
         fastify.log.error(err)
         process.exit(1)
     }
