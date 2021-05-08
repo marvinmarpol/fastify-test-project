20c3d1b5a808c9fbf3da6e0fba881f6f844c6b3c
marvin <marvin.mitchell@amarbank.co.id>
2021-05-08 13:26:13 +0700

    2-typescript-support: add code changes

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-// Require the framework and instantiate it
-const fastify = require("fastify")({ logger: true })
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+const server: FastifyInstance = Fastify({})
+
+const opts: RouteShorthandOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    pong: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    }
+}
+
+server.get('/ping', opts, async (request, reply) => {
+    return { pong: 'it worked' }
 })
 
-// Run the server!
-const start = async()=>{
-    try{
-        await fastify.listen(3000)
-    } catch(err){
-        fastify.log.error(err)
+const start = async () => {
+    try {
+        await server.listen(3000)
+
+        const address = server.server.address()
+        const port = typeof address === 'string' ? address : address?.port
+    } catch (err) {
+        server.log.error(err)
         process.exit(1)
     }
 }
-start()
