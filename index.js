b4d05cdd7dd8b5832985b16adf1160feee0b91a5
marvinmitchell <marvin.mitchell@amarbank.co.id>
2021-05-14 12:04:24 +0700

    5-initial-config: add example of fastify initial config

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 // Require the framework and instantiate it
-const fastify = require("fastify")({ logger: true })
+const fastify = require("fastify")({
+    /* https: {
+        allowHTTP1: true,
+        key: readFileSync('./fastify.key'),
+        cert: readFileSync('./fastify.cert')
+    }, */
+    logger: { level: 'trace' },
+    ignoreTrailingSlash: true,
+    maxParamLength: 200,
+    caseSensitive: true,
+    //trustProxy: '127.0.0.1,192.168.1.1/24',
+})
+fastify.log.trace(fastify.initialConfig)
+
+fastify.register(async (instance, opts) => {
+    instance.get('/initial-config', async (request, reply) => {
+        return instance.initialConfig
+    })
+
+    instance.get('/error', async (request, reply) => {
+        // will throw an error because initialConfig is read-only
+        // and can not be modified
+        instance.initialConfig.https.allowHTTP1 = false
+        return instance.initialConfig
+    })
+})
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
@@ -7,10 +32,10 @@ fastify.get('/', async (request, reply) => {
 })
 
 // Run the server!
-const start = async()=>{
-    try{
+const start = async () => {
+    try {
         await fastify.listen(3000)
-    } catch(err){
+    } catch (err) {
         fastify.log.error(err)
         process.exit(1)
     }
