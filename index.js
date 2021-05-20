a88c90da99cfa3b8d04c1fbf10e98ae821569e86
unknown <marvinmarpol@gmail.com>
2021-05-21 00:15:54 +0700

    8-log-redaction: add example filtering secret header in logs

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // Require the framework and instantiate it
-const fastify = require("fastify")({ logger: true })
+const fastify = require("fastify")({
+    logger: {
+        redact: ['req.headers.authorization'],
+        level: 'trace',
+        serializers: {
+            req(request) {
+                return {
+                    method: request.method,
+                    url: request.url,
+                    headers: request.headers,
+                    hostname: request.hostname,
+                    remoteAddress: request.ip,
+                    remotePort: request.socket.remotePort
+                }
+            }
+        }
+    }
+})
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
@@ -7,10 +24,10 @@ fastify.get('/', async (request, reply) => {
 })
 
 // Run the server!
-const start = async()=>{
-    try{
+const start = async () => {
+    try {
         await fastify.listen(3000)
-    } catch(err){
+    } catch (err) {
         fastify.log.error(err)
         process.exit(1)
     }
