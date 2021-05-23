3e714c7dcc9aafa4aca83e62593379338dcd384d
marvinmitchell <marvin.mitchell@amarbank.co.id>
2021-05-23 17:23:53 +0700

    12-fluent-schema: add fluent schema example

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+// https://www.fastify.io/docs/latest/Fluent-Schema/
+
+// require fluent schema
+const S = require('fluent-json-schema')
+
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true })
 
-// Declare a route
+const addressSchema = S.object()
+  .id('#address')
+  .prop('line1').required()
+  .prop('line2')
+  .prop('country').required()
+  .prop('city').required()
+  .prop('zipcode').required()
+
+const commonSchemas = S.object()
+  .id('https://fastify/demo')
+  .definition('addressSchema', addressSchema)
+  //.definition('otherSchema', otherSchema) // You can add any schemas you need
+
+fastify.addSchema(commonSchemas)
+
+const bodyJsonSchema = S.object()
+  .prop('residence', S.ref('https://fastify/demo#address')).required()
+  .prop('office', S.ref('https://fastify/demo#/definitions/addressSchema')).required()
+
+const schema = { body: bodyJsonSchema }
+
+// Declare routes
+fastify.post('/the/url', { schema }, (request, reply) => {
+    return { hello: 'world' }
+})
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
 })
