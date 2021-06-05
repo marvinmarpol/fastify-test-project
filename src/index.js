e739c0a28a17a94ac3a929c019a7d45b5b0ce1e4
marvin <marvin.mitchell@amarbank.co.id>
2021-06-06 04:20:53 +0700

    development: change default host

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ fastify.register(require('./modules/user/route'));
 // Run the server
 const start = async () => {
     try {
-        await fastify.listen(config.app.port);
+        await fastify.listen(config.app.port, '0.0.0.0');
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
