04b1452f3cb4066b0c2ee82c7482afecbd4d7bb7
marvinmitchell <marvin.mitchell@amarbank.co.id>
2021-06-06 07:05:32 +0700

    clean-architecture: add error and apm monitoring

--- src/modules/user/controller.js
+++ src/modules/user/controller.js
@@ -1,16 +1,18 @@
 const usecase = require('./usecase');
+const Sentry = require("@sentry/node");
 const { StatusCodes } = require('http-status-codes');
 
 const checkUser = async (request, reply) => {
     try {
         data = await usecase.checkUser(request.params.id);
+        if (!data) {
+            return reply.code(StatusCodes.NOT_FOUND).send(null);
+        }
+        reply.send(data);
     } catch (err) {
+        Sentry.captureException(err)
         reply.code(err.getHttpCode()).send(err);
     }
-    if (!data) {
-        reply.code(StatusCodes.NOT_FOUND).send(data);
-    }
-    reply.send(data);
 }
 
 module.exports = Object.freeze({
